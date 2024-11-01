9e5aadce0a55c78b8f2206dbe0dd7d994ea6455a
Gabriel Munthali <gabmunthali@gmail.com>
2024-11-01 09:35:04 +0200

    Chapter 39

---
+++ 39. Char Datatype and UTF16 Smiley/MyApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char myFavoriteCharacter = '☺';
+
+        Console.WriteLine(myFavoriteCharacter);
+        
+        
+        //Operators and Order of evaluation
+        int num1 = 14;
+        Console.WriteLine("Please enter a whole number !");
+        int num2 = int.Parse(Console.ReadLine());
+
+        //Concatination
+        Console.WriteLine("Age is " + num1);
+
+
+        Console.WriteLine("Addition num1 + num2 = " + (num1 + num2));
+        //Order of evaluation
+        Console.WriteLine("Subtraction num1 - num2 = " + (num1 - num2));
+
+        Console.WriteLine("Division num1 / num2 = " + num1 / num2);
+    }
+}
