9e6f0e03ac618883fc4b4f1bc174c1626b6e8a44
Gabriel Munthali <gabmunthali@gmail.com>
2024-11-01 10:58:42 +0200

    40. More String Manipulation with String Formatting

---
+++ 40. More String Manipulation with String Formatting/MyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 10;
+        double price =  10.99;
+        string name =  "John";
+
+        //Interpolation
+        Console.WriteLine($"The number is {num}");
+
+        //Concatenation
+        Console.WriteLine("The number is " + num);
+
+        //String formatting
+        Console.WriteLine("The number is {0}, and the price is {1}, and the name is {2}",  num, price, name);
+
+
+        Console.ReadKey();
+    }
+}
