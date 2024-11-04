ef8b26f20149e8f962926ac9845f1b37b59e83e4
Gabriel Munthali <gabmunthali@gmail.com>
2024-11-04 10:32:09 +0200

    49. If statements

---
+++ 49. If Statements/MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isRainy = true;
+        bool hasUmbrella = true;
+
+        if (isRainy)
+        {
+            Console.WriteLine("Its rainy");
+        }
+
+        Console.WriteLine("Ay okay");
+    }
+}
