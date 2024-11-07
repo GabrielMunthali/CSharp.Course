3a89222a9fe32eba33b4d46812a3935177318c9a
Gabriel Munthali <gabmunthali@gmail.com>
2024-11-07 08:28:37 +0200

    51. And operator

---
+++ 51. AND operator/MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isRainy = true;
+        bool hasUmbrella = false;
+
+        if (isRainy && !hasUmbrella)
+        {
+            Console.WriteLine("I am getting wet");
+        }
+
+        Console.WriteLine("Ay ok !");
+    }
+}
