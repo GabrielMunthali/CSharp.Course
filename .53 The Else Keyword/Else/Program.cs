7455ca4889cc556eeb6d3f4463e9d04dd0f5f1cd
Gabriel Munthali <gabmunthali@gmail.com>
2024-11-08 09:30:25 +0200

    .53 The Else Keyword

---
+++ .53 The Else Keyword/Else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Else;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 6;
+
+        bool isGreater = num1 > num2; // Relational operator
+
+        int age = 17;
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Go party");
+        } else 
+            {
+                Console.WriteLine("No");
+            }
+
+    }
+}
