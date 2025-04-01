d3c5035db824c02b6aa6ca14a0b7c008d555e6aa
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-01 11:03:05 +0200

    58. Equality operator and not equal

---
+++ 58. Equality operator and not equal/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = 0;
+int num2 = 0;
+
+bool isEqual = num1 == num2;
+
+bool isNotEqual = num1 != num2;
+
+Console.WriteLine("Please enter a whole number");
+
+if (num1 == int.Parse(Console.ReadLine()))
+{
+    Console.WriteLine("Numbers are equal");
+}
+else
+{
+    Console.WriteLine("Numbers are not equal");
+}
