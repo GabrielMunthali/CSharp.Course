792019de803c80e2c03b1651ef883ee1b9fa6879
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-01 10:47:43 +0200

    57. Else if keyword and order of importance

---
+++ 57. Else if keyword and order of importance/Program.cs
@@ -0,0 +1,32 @@
+﻿//bool isRainy = true;
+//bool hasUmbrella = false;
+
+//if (isRainy && !hasUmbrella)
+//{
+//    Console.WriteLine("I am getting wet");
+//}
+
+//Console.WriteLine("Ay ok !");
+
+
+//int num1 = 5;
+//int num2 = 6;
+
+//bool isGreater = num1 > num2; // Relational operator
+
+int age = 16;
+bool isWithParents = false;
+
+if (age >= 13 && isWithParents)
+{
+    Console.WriteLine("Go party in the club with your parents");
+} else if (age > 6)
+{
+    Console.WriteLine("Go party in club");
+}
+else
+{
+    Console.WriteLine("Go party in kindergarten");
+}
+
+Console.ReadLine();
