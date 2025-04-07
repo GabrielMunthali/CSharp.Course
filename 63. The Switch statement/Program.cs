6d8c89c2f6667d2695fbd2bda7ed6a3012453ab1
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-07 14:16:04 +0200

    64. The switch statement keyword switch

---
+++ 63. The Switch statement/Program.cs
@@ -0,0 +1,31 @@
+﻿int month = 5;
+string monthName;
+
+if (month == 1)
+{
+    monthName = "January";
+}
+else if (month == 2)
+    monthName = "February";
+else if (month == 3)
+    monthName = "March";
+else
+    monthName = "Unknown";
+
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+    case 2:
+        monthName = "February";
+        break;
+    case 3:
+        monthName = "March";
+        break;
+    default:
+        monthName = "Unknown";
+        break;
+}
+
+Console.WriteLine($"The month name is {monthName}");
