1a23625361e0462ed5a21dbff63a75cfd9a6d527
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-16 09:47:40 +0200

    68. Incrementing and pre incrementing

---
+++ 68. Incrementing and preincrementing/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = 0;
+
+Console.WriteLine($"Num is {num}");
+num++;
+
+Console.WriteLine($"Num is {num}");
+
+
+Console.WriteLine("Num is {0}", ++num);
+Console.WriteLine("Num is {0}", num++);
+Console.WriteLine("Num is {0}", num);
