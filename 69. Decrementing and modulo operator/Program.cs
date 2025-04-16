ee7439981f106c6c52652b52e7cd3f3a87d51931
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-16 10:41:19 +0200

    69. Decrementing and modulo operator

---
+++ 69. Decrementing and modulo operator/Program.cs
@@ -0,0 +1,27 @@
+﻿int num1 = 10;
+int num2 = 3;
+
+int result = num1 % num2;
+Console.WriteLine(result);
+
+
+// num++;
+
+//Console.WriteLine($"Num is {num}");
+
+
+//Console.WriteLine("Num is {0}", ++num);
+//Console.WriteLine("Num is {0}", num++);
+//Console.WriteLine("Num is {0}", num);
+
+// num--;
+//Console.WriteLine("Num is {0}", num);
+//Console.WriteLine("Num is {0}", num--);
+//Console.WriteLine("Num is {0}", num);
+
+// num += 30;
+// Console.WriteLine("Num is {0}", num);
+// num -= 10;
+// Console.WriteLine("Num is {0}", num);
+//
+// Console.ReadLine();
