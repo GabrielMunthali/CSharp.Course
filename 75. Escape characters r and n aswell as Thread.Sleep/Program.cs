8cc93f2a400ed9e85235af049d590e74864c437c
Gabriel Munthali <gabmunthali@gmail.com>
2025-05-19 09:19:00 +0200

    75. Escape characters r and n aswell as Thread.Sleep

---
+++ 75. Escape characters r and n aswell as Thread.Sleep/Program.cs
@@ -0,0 +1,12 @@
+﻿string myString = "Hie \r\nHie";
+
+
+
+for (int counter = 10; counter >= 0; counter--)
+{
+    Console.WriteLine("Counter is " + counter);
+    Console.WriteLine(myString);
+    Thread.Sleep(1000);
+}
+
+Console.ReadKey();
